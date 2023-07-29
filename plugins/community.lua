32965fb78e27e829b6972febf4c6f44b014c8833
doman <domanski@basement.pl>
2023-07-29 18:13:09 +0200

    added some lsps

--- plugins/community.lua
+++ plugins/community.lua
@@ -13,4 +13,9 @@ return {
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.dart" },
 }
